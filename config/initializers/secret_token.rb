f2cdb630e58af0d7d680250d95871d00963a7804
Neill Bogie <neillbogie@googlemail.com>
2011-07-09 11:54:23 +0100

    Initial commit. rails 3.0.9.  added .gitkeep files and ignore database.yml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reportapp::Application.config.secret_token = '1917c7f937d42e63501dc25c1f5cd7faee9c33a5567b283d69d8da66e1d81ef710d490248ffc6483a21c18570d79c679252debe25dc907321b21c252b9bdd1c1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Reportapp::Application.config.session_store :cookie_store, :key => '_reportapp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Reportapp::Application.config.session_store :active_record_store
